e4de03f9a1169ade78b69e5236779f8e35f4824a
Lionel <lionelpinau78570@gmail.com>
2025-04-17 11:15:19 +0200

    ignore eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,8 +12,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    name: "custom-rules",
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
 ];
